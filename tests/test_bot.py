01bafa402ad39fc0c2652faea3aac80c8a54a13b
Orion <humphreyorion@gmail.com>
2022-01-20 00:56:11 -0700

    Changed workflows to run on test branch and on pull requests. Added temp test files with one passesing test.

---
+++ tests/test_bot.py
@@ -0,0 +1,19 @@
+# import unittest
+# import process_tags
+# import bot
+
+# class Test_process_tags(unittest.TestCase()):
+#     def test_get_webpage(self):
+#         self.assertRaises(Exception,get_webpage,"http://asdf.sasfd/")
+# class Test_bot(unittest.TestCase()):
+#     pass
+# class Test_headers(unittest.TestCase()):
+#     pass
+# if __name__ == '__main__':
+#     unittest.main()
+
+def test_always_passes():
+    assert(True)
+
+# def test_always_fails():
+#     assert(False)
