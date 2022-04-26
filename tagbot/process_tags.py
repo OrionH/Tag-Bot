9c091d055b247ac30a250921c5243ad441969d74
Orion <humphreyorion@gmail.com>
2022-04-26 02:37:12 -0600

    Fixed lint errors in dpendabot.yml, added request_file to requirements for testing, put tagbot files in their own folder, added test.html for testing. Wrote tests for process_tags.py.

---
+++ tagbot/process_tags.py
@@ -0,0 +1,165 @@
+"""
+=====================================================================
+@author:        Orion Humphrey
+Project:     	Discord Tag Bot
+Date:           Nov 11, 2021
+Version:        1
+Description:    Methods to process keyword tags for the Discord bot.
+Notes:
+=====================================================================
+"""
+
+# Used by BeautifulSoup to speed up parsing
+import cchardet
+import nltk
+from bs4 import BeautifulSoup
+
+# Download required packages for processing
+nltk.download("punkt")
+nltk.download("averaged_perceptron_tagger")
+
+
+def scrape(response) -> list:
+    """Function to scrape the title and entire text of a web page.
+
+    Args:
+        url (str): URL to scrape
+        header (str): User agent information
+
+    Returns:
+        list: Returns strings of the scraped title and text.
+    """
+    # Scrape webpage with lxml parser because it is often the fastest parser
+    scraped_content = BeautifulSoup(response.content, "lxml")
+    # Get string of first head > title on page
+    title = scraped_content.find("head").find("title").get_text()
+    # Get text from entire webpage
+    text = scraped_content.get_text()
+
+    return [title, text]
+
+
+def count_occurrences(word_list: list) -> list:
+    """Function to count the occurrence of each word in a body of text.
+    Words get sorted from most to least frequent and any word that doesn't
+    occur more than twice gets removed. The most frequent words are returned
+    with a minimum of zero and a maximum of seven.
+
+    Args:
+        word_list (list): Takes in list of tuples (str,str). Words tagged with
+                          parts of speech.
+
+    Returns:
+        list: Final tags (strings).
+    """
+    # Create dictionary of words with the default value of 0
+    word_dict = {word_list[0]: 0 for word_list in word_list}
+    # If there is more than one occurrence of a word, add 1 to the value
+    for i in word_list:
+        if i[0] in word_dict:
+            word_dict[i[0]] += 1
+
+    # NOTE Use for seeing number of word occurrences
+    # print(sorted(word_dict.items(), key=lambda x: x[1], reverse=True))
+
+    # Delete any keys that don't have a value greater than 2 unless tag list
+    # is short (<3 tags). This reduces the tags to important ones.
+    for key, val in list(word_dict.items()):
+        if val <= 2:
+            del word_dict[key]
+
+    # Generate list from dictionary of words sorted from most occurrences to least
+    tags = sorted(word_dict, key=word_dict.get, reverse=True)
+    # Return 7 tags
+    return tags[:7]
+
+
+def sort_title(words: list) -> list:
+    """Function to sort through text and sort the words by parts of speech.
+    A string of text from a title is sorted to have propper nouns at the
+    beginning of the list, then regular nouns, and then adjectives. The
+    words returned can range from a minimum of zero to a maximum of three.
+
+    Args:
+        words (list): Takes in list of tuples (str,str). Words tagged with
+                      parts of speech.
+
+    Returns:
+        list: Final tags (strings).
+    """
+    order = ["NNPS", "NNP", "NNS", "NN", "JJS"]
+    # Sort list of words by the oder list.
+    # This is to put more relevant tags (IMO) in the beginning of the list.
+    sorted_tags = sorted(words, key=lambda i: order.index(i[1]))
+    # Remove parts of speech tags
+    sorted_words = [word[0] for word in sorted_tags]
+    # Only return 3 title tags
+    return sorted_words[:3]
+
+
+def process_words(words: str) -> list:
+    """Function to tokenize a string, tag the words with parts of speech,
+    remove punctuation and excessively long words (often junk words), and
+    remove all parts of speech not desired.
+
+    Args:
+        words (str): Takes in a string of text.
+
+    Returns:
+        list: List of tuples (str,str). Words tagged with parts of speech.
+    """
+    # list of NLTK parts of speech for creating tags.
+    # NN noun, singular "desk", NNS noun plural "desks", NNP proper noun,
+    # singular "Harrison", NNPS proper noun, plural "dogs", JJS adjective, superlative "biggest"
+    parts_of_speech = ["NN", "NNS", "NNP", "NNPS", "JJS"]
+
+    # Tokenize text
+    tok_list = nltk.word_tokenize(words)
+    # Tag words in list with their parts of speech
+    tagged_word_list = nltk.pos_tag(tok_list)
+    # Remove every word that does not match the parts of speech in the parts of speech list
+    reduced_tagged_list = [
+        word for word in tagged_word_list if word[1] in parts_of_speech]
+    # Capitalization affects the tagging process so this comes last.
+    # convert words to lower case and remove punctuation and numbers.
+    # Also remove words longer than 21 characters "Incomprehensibilities"
+    final_tagged_list = [(word[0].lower(), word[1])
+                 for word in reduced_tagged_list if word[0].isalpha() and len(word[0]) < 21]
+
+    return final_tagged_list
+
+
+def create_tags(text:list) -> str:
+    """Function to process a web page into keywords from that page. Keyword
+    tags are sorted by importance. Up to three tags from the title of the page
+    in propper noun, noun, adjective order. Up to seven tags from the entire
+    page in most frequent to least frequent order.
+
+    Args:
+        list[str,str]: List of strings containing title and body text
+
+    Returns:
+        str: Formatted string of keywords or tags
+    """
+    # Title
+    title_tags = sort_title(process_words(text[0]))
+    # Text
+    text_tags = count_occurrences(process_words(text[1]))
+
+    # Remove duplicates and prioritize title tag location
+    for i in title_tags:
+        if i in text_tags:
+            text_tags.remove(i)
+
+    # Title and main body text tags combined
+    final_tag_list = title_tags + text_tags
+
+    if len(final_tag_list) == 0:
+        return "No tags were found."
+
+    # Capitalize first letter for looks
+    final_tag_list = [word.capitalize() for word in final_tag_list]
+    # Convert to string
+    final_tags = ", ".join(final_tag_list)
+
+    return final_tags
--- tests/test_process_tags.py
+++ tests/test_process_tags.py
@@ -2,9 +2,203 @@
 =====================================================================
 @author:        Orion Humphrey
 Project:     	Discord Tag Bot
-Date:           Jan 26, 2022
+Date:           Apr 25, 2022
 Version:        1
 Description:    Methods to test processing of keywords.
-Notes:
+Notes:          I'm not checking input validation.
 =====================================================================
 """
+import os
+import sys
+
+import requests
+from requests_file import FileAdapter
+from tagbot.process_tags import (
+    scrape,
+    count_occurrences,
+    sort_title,
+    process_words,
+    create_tags,
+)
+
+# Uncomment for local testing. Needs to go above tagbot imports.
+# Pytest adds project folder to path when using python -m pytest.
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+
+def test_scrape():
+    """Function to test the scraping of a title and text from a webpage
+    response object.
+    """
+    session = requests.Session()
+    session.mount("file://", FileAdapter())
+
+    path = os.path.dirname(os.path.realpath(__file__))
+    path = path.replace("\\", "/")
+
+    response = session.get("file:///" + path + "/test.html")
+
+    assert scrape(response) == [
+        "This is the title to be scraped.",
+        "\n\nThis is the title to be scraped.\n\nThis is the body of the html file to be scraped.\n",
+    ]
+
+
+def test_count_occurrences():
+    """Function to test oder tagged words by frequency and remove non-
+    important words.
+    """
+
+    # Empty input
+    tag_list1 = []
+    # Multiple occurrences of words
+    tag_list2 = [
+        ("text", "NN"),
+        ("body", "NN"),
+        ("webpage", "NN"),
+        ("word", "NN"),
+        ("webpage", "NN"),
+        ("times", "NNS"),
+        ("webpage", "NN"),
+        ("tags", "NN"),
+        ("instances", "NNS"),
+        ("text", "NN"),
+        ("tags", "NN"),
+        ("webpage", "NN"),
+        ("body", "NN"),
+        ("tags", "NNS"),
+    ]
+    # One of each parts of speech
+    tag_list3 = [
+        ("text", "NN"),
+        ("us", "NNP"),
+        ("americans", "NNPS"),
+        ("undergraduates", "NNS"),
+        ("best", "JJS"),
+    ]
+
+    assert count_occurrences(tag_list1) == []
+    assert count_occurrences(tag_list2) == ["webpage", "tags"]
+    assert count_occurrences(tag_list3) == []
+
+
+def test_sort_title():
+    """Function to test word odering from tagged text. Tests for empty list."""
+
+    # Empty input
+    tag_list1 = []
+    # Out of order tagged words
+    tag_list2 = [
+        ("americans", "NNPS"),
+        ("text", "NN"),
+        ("best", "JJS"),
+        ("undergraduates", "NNS"),
+        ("us", "NNP"),
+    ]
+    # Out of oder tagged parts of speech that weren't in tag_list2
+    tag_list3 = [("biggest", "JJS"), ("ask", "NN"), ("hers", "NNS")]
+
+    assert sort_title(tag_list1) == []
+    assert sort_title(tag_list2) == ["americans", "us", "undergraduates"]
+    assert sort_title(tag_list3) == ["hers", "ask", "biggest"]
+
+
+def test_process_words():
+    """Function to test the processing of words function. This tagging parts
+    of speed, empty strings, text where each word only occurs once, long
+    words that should be filtered out, removing words with numbers or
+    symbols, and filtering parts of speech.
+    """
+
+    # Empty input
+    text1 = ""
+    # Regular example of an input
+    text2 = """This is test text to simulate a body of a webpage. I will use
+     the word webpage multiple times so webpage shows up in the tags. There
+     will also be instances of, " text ", " tags ", " webpage ", " body " and " tags "
+     again."""
+    # Each word only occurs once
+    text3 = "This text has each word only occuring one time."
+    # Capitalization affects tagging.
+    # Test with all parts of speech able to be tagged by nltk.
+    text4 = """and but & , () [] {} ' " US — . ! ? : ; ... one-tenth another
+     there gemeinshaft among out on pre-war braver closer calmest closest AA. F
+     can dare  shed slide Escobar Shannon Americans Angels undergraduates
+     clubs all both John's 's hers me one mine our occasionally swiftly out
+     apart % * = U.S.S.R to Dang Jeepers further grander best biggest about
+     ask avoid dipped soaked focusing judging factored primed wrap grab
+     seals weaves that which who whose how where
+    """
+    # Words that contain or are numbers
+    text5 = "Text with numbers 1 12 0 98 4567 234-2 4,956 6.78"
+    # Super long words
+    text6 = "Incomprehensibilities Pneumonoultramicroscopicsilicovolcanoconiosis Thyroparathyroidectomized"
+
+    assert process_words(text1) == []
+    assert process_words(text2) == [
+        ("text", "NN"),
+        ("body", "NN"),
+        ("webpage", "NN"),
+        ("word", "NN"),
+        ("webpage", "NN"),
+        ("times", "NNS"),
+        ("webpage", "NN"),
+        ("tags", "NN"),
+        ("instances", "NNS"),
+        ("text", "NN"),
+        ("webpage", "NN"),
+        ("body", "NN"),
+    ]
+    assert process_words(text3) == [("text", "NN"), ("word", "NN"), ("time", "NN")]
+    assert process_words(text4) == [
+        ("us", "NNP"),
+        ("gemeinshaft", "NN"),
+        ("braver", "NN"),
+        ("calmest", "NN"),
+        ("closest", "NN"),
+        ("aa", "NNP"),
+        ("f", "NNP"),
+        ("escobar", "NNP"),
+        ("shannon", "NNP"),
+        ("americans", "NNPS"),
+        ("angels", "NNPS"),
+        ("undergraduates", "NNS"),
+        ("john", "NNP"),
+        ("hers", "NNS"),
+        ("dang", "NNP"),
+        ("jeepers", "NNP"),
+        ("grander", "NN"),
+        ("best", "JJS"),
+        ("biggest", "JJS"),
+        ("ask", "NN"),
+        ("avoid", "NN"),
+        ("judging", "NN"),
+        ("wrap", "NN"),
+        ("grab", "NN"),
+        ("seals", "NNS"),
+    ]
+    assert process_words(text5) == [("text", "NN"), ("numbers", "NNS")]
+    assert process_words(text6) == []
+
+
+def test_create_tags():
+    """Function to test the creation of a string of tags from a list of tagged
+    words generated from a title and body of text. Tests for empty tags.
+    """
+
+    # Empty input
+    text1 = ["", ""]
+    # Duplicate tags in title and body
+    text2 = [
+        "This is a title",
+        "This is a body of text. It has duplicate words like title title tile and text text text to check for duplicate removal and adding one tag from the body.",
+    ]
+    # Text only in body
+    text3 = ["", "This test only has a body. Test test test."]
+    # Text only in title
+    text4 = ["This test only has a title. Title title title.", ""]
+
+    assert create_tags(text1) == "No tags were found."
+    assert create_tags(text2) == "Title, Text"
+    assert create_tags(text3) == "Test"
+    assert create_tags(text4) == "Title, Test, Title"
